6af2bd6ecd78d21241ca42ab38088e2152891520
LeonardoLamar12 <Leonardocruzlamari@gmail.com>
2025-11-13 23:51:24 -0300

    Prototipo final

---
+++ src/Prototipo final/MasterScore/FormPergunta.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace MasterScore
+{
+    partial class FormPergunta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtPergunta = new TextBox();
+            btnResA = new Button();
+            lblResA = new Label();
+            lblResB = new Label();
+            btnResB = new Button();
+            lblResD = new Label();
+            btnResD = new Button();
+            lblResC = new Label();
+            btnResC = new Button();
+            btnPerguntar = new Button();
+            btnLoja = new Button();
+            SuspendLayout();
+            // 
+            // txtPergunta
+            // 
+            txtPergunta.BackColor = SystemColors.MenuText;
+            txtPergunta.ForeColor = SystemColors.Window;
+            txtPergunta.Location = new Point(33, 22);
+            txtPergunta.Multiline = true;
+            txtPergunta.Name = "txtPergunta";
+            txtPergunta.Size = new Size(717, 103);
+            txtPergunta.TabIndex = 0;
+            txtPergunta.TextChanged += txtPergunta_TextChanged;
+            // 
+            // btnResA
+            // 
+            btnResA.Location = new Point(33, 177);
+            btnResA.Name = "btnResA";
+            btnResA.Size = new Size(75, 23);
+            btnResA.TabIndex = 1;
+            btnResA.Text = "A)";
+            btnResA.UseVisualStyleBackColor = true;
+            btnResA.Click += btnResA_Click;
+            // 
+            // lblResA
+            // 
+            lblResA.AutoSize = true;
+            lblResA.Location = new Point(114, 181);
+            lblResA.Name = "lblResA";
+            lblResA.Size = new Size(65, 15);
+            lblResA.TabIndex = 3;
+            lblResA.Text = "Resposta A";
+            // 
+            // lblResB
+            // 
+            lblResB.AutoSize = true;
+            lblResB.Location = new Point(114, 232);
+            lblResB.Name = "lblResB";
+            lblResB.Size = new Size(64, 15);
+            lblResB.TabIndex = 5;
+            lblResB.Text = "Resposta B";
+            // 
+            // btnResB
+            // 
+            btnResB.Location = new Point(33, 228);
+            btnResB.Name = "btnResB";
+            btnResB.Size = new Size(75, 23);
+            btnResB.TabIndex = 4;
+            btnResB.Text = "B)";
+            btnResB.UseVisualStyleBackColor = true;
+            btnResB.Click += btnResB_Click;
+            // 
+            // lblResD
+            // 
+            lblResD.AutoSize = true;
+            lblResD.Location = new Point(114, 337);
+            lblResD.Name = "lblResD";
+            lblResD.Size = new Size(65, 15);
+            lblResD.TabIndex = 9;
+            lblResD.Text = "Resposta D";
+            // 
+            // btnResD
+            // 
+            btnResD.Location = new Point(33, 333);
+            btnResD.Name = "btnResD";
+            btnResD.Size = new Size(75, 23);
+            btnResD.TabIndex = 8;
+            btnResD.Text = "D)";
+            btnResD.UseVisualStyleBackColor = true;
+            btnResD.Click += btnResD_Click;
+            // 
+            // lblResC
+            // 
+            lblResC.AutoSize = true;
+            lblResC.Location = new Point(114, 286);
+            lblResC.Name = "lblResC";
+            lblResC.Size = new Size(65, 15);
+            lblResC.TabIndex = 7;
+            lblResC.Text = "Resposta C";
+            // 
+            // btnResC
+            // 
+            btnResC.Location = new Point(33, 282);
+            btnResC.Name = "btnResC";
+            btnResC.Size = new Size(75, 23);
+            btnResC.TabIndex = 6;
+            btnResC.Text = "C)";
+            btnResC.UseVisualStyleBackColor = true;
+            btnResC.Click += btnResC_Click;
+            // 
+            // btnPerguntar
+            // 
+            btnPerguntar.Location = new Point(583, 142);
+            btnPerguntar.Name = "btnPerguntar";
+            btnPerguntar.Size = new Size(167, 23);
+            btnPerguntar.TabIndex = 10;
+            btnPerguntar.Text = "Pedir pergunta";
+            btnPerguntar.UseVisualStyleBackColor = true;
+            btnPerguntar.Click += btnPerguntar_Click;
+            // 
+            // btnLoja
+            // 
+            btnLoja.Location = new Point(674, 415);
+            btnLoja.Name = "btnLoja";
+            btnLoja.Size = new Size(114, 23);
+            btnLoja.TabIndex = 11;
+            btnLoja.Text = "Ir para loja";
+            btnLoja.UseVisualStyleBackColor = true;
+            btnLoja.Click += btnLoja_Click;
+            // 
+            // FormPergunta
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnLoja);
+            Controls.Add(btnPerguntar);
+            Controls.Add(lblResD);
+            Controls.Add(btnResD);
+            Controls.Add(lblResC);
+            Controls.Add(btnResC);
+            Controls.Add(lblResB);
+            Controls.Add(btnResB);
+            Controls.Add(lblResA);
+            Controls.Add(btnResA);
+            Controls.Add(txtPergunta);
+            Name = "FormPergunta";
+            Text = "FormPergunta";
+            Load += FormPergunta_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtPergunta;
+        private Button btnResA;
+        private Label lblResA;
+        private Label lblResB;
+        private Button btnResB;
+        private Label lblResD;
+        private Button btnResD;
+        private Label lblResC;
+        private Button btnResC;
+        private Button btnPerguntar;
+        private Button btnLoja;
+    }
+}
---
+++ src/Prototipo final/MasterScore/FormsLogin.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace MasterScore
+{
+    partial class FormsLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnEnviar = new Button();
+            txtUsuario = new TextBox();
+            txtSenha = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // btnEnviar
+            // 
+            btnEnviar.Location = new Point(358, 276);
+            btnEnviar.Name = "btnEnviar";
+            btnEnviar.Size = new Size(75, 23);
+            btnEnviar.TabIndex = 0;
+            btnEnviar.Text = "Login";
+            btnEnviar.UseVisualStyleBackColor = true;
+            btnEnviar.Click += btnEnviar_Click;
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(324, 123);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(153, 23);
+            txtUsuario.TabIndex = 1;
+            // 
+            // txtSenha
+            // 
+            txtSenha.Location = new Point(324, 190);
+            txtSenha.Name = "txtSenha";
+            txtSenha.Size = new Size(153, 23);
+            txtSenha.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(324, 105);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Usuario:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(324, 172);
+            label2.Name = "label2";
+            label2.Size = new Size(42, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Senha:";
+            // 
+            // FormsLogin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtSenha);
+            Controls.Add(txtUsuario);
+            Controls.Add(btnEnviar);
+            Name = "FormsLogin";
+            Text = "Form1";
+            Load += FormsLogin_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnEnviar;
+        private TextBox txtUsuario;
+        private TextBox txtSenha;
+        private Label label1;
+        private Label label2;
+    }
+}
